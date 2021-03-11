a59d311ea4d9b377119fbaa31aa35d5d13333f88
Rafael Delgado <rafa12del@hotmail.com>
2021-03-11 19:39:16 +0100

    Repositories

---
+++ DecodeOficial.Domain/Interfaces/Repositories/IRepositoryPerson.cs
@@ -0,0 +1,8 @@
+﻿using DecodeOficial.Domain.Entities;
+
+namespace DecodeOficial.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryPerson : IRepositoryBase<Person>
+    {
+    }
+}
---
+++ DecodeOficial.Infrastructure.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,81 @@
+﻿using DecodeOficial.Domain.Interfaces.Repositories;
+using DecodeOficial.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecodeOficial.Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly DecodeContext _decodeContext;
+
+        public RepositoryBase(DecodeContext decodeContext)
+        {
+            _decodeContext = decodeContext;
+        }
+
+        public void Add(T obj)
+        {
+            try
+            {
+                _decodeContext.Set<T>().Add(obj);
+                _decodeContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            try
+            {
+                return _decodeContext.Set<T>().ToList();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public T GetById(int id)
+        {
+            try
+            {
+                return _decodeContext.Set<T>().Find(id);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Remove(T obj)
+        {
+            try
+            {
+                _decodeContext.Set<T>().Remove(obj);
+                _decodeContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Update(T obj)
+        {
+            try
+            {
+                _decodeContext.Entry(obj).State = EntityState.Modified;
+                _decodeContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
